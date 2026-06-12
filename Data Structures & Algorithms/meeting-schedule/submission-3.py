4203d72c1bb20b257e0be6a7c704f26a22604f5f
MuddyHope <60110059+MuddyHope@users.noreply.github.com>
2026-06-12 17:32:09 +0530

    Add: meeting-schedule - submission-3

---
+++ Data Structures & Algorithms/meeting-schedule/submission-3.py
@@ -0,0 +1,29 @@
+"""
+Definition of Interval:
+class Interval(object):
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+"""
+
+class Solution:
+    def canAttendMeetings(self, intervals: List[Interval]) -> bool:
+        
+        if not intervals:
+            return True
+        
+        intervals = sorted(intervals, key= lambda x: x.start)
+    
+        _start = intervals[0].start
+        _endtime = intervals[0].end
+
+        for meeting in intervals[1:]:
+            # print(f"endtime: {_endtime}, starttime: {meeting.start}")
+            if meeting.start < _endtime:
+                return False
+            _endtime = meeting.end
+        return True
+
+
+
+            
